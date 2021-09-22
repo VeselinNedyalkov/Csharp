b0d8fb7adf7e5c9dc75c4255d2663c583a2802d8
Veseli Nedyalkov <91067756+VeselinNedyalkov@users.noreply.github.com>
2021-09-22 20:24:50 +0300

    Update 03. Elevator.cs

--- Fundamentals/Data Types and Variables - Exercise/03. Elevator.cs
+++ Fundamentals/Data Types and Variables - Exercise/03. Elevator.cs
@@ -7,22 +7,14 @@ namespace _03.Elevator
     {
         static void Main(string[] args)
         {
-            int numPeople = int.Parse(Console.ReadLine());
-            int elevatorCap = int.Parse(Console.ReadLine());
+            double numPeople = int.Parse(Console.ReadLine());
+            double elevatorCap = int.Parse(Console.ReadLine());
             double courses = 0;
 
             courses = numPeople / elevatorCap;
             courses = Math.Ceiling(courses);
 
-            if (numPeople <= elevatorCap)
-{
-                Console.WriteLine($"All the persons fit inside in the elevator.{Environment.NewLine}Only one course is needed.");
-            }
-            else
-            {
-                Console.WriteLine($"{courses} courses * {elevatorCap} people{Environment.NewLine}+ 1 course * {numPeople % elevatorCap} persons");
-            }
-
+             Console.WriteLine(courses);                      
         }
     }
 }
