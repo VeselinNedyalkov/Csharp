f8848959344bd943a7b9e28a186867ebf1e23094
Veseli Nedyalkov <91067756+VeselinNedyalkov@users.noreply.github.com>
2021-10-11 12:31:57 +0300

    Create 01. Smallest of Three Numbers.cs

---
+++ Fundamentals/Methods/01. Smallest of Three Numbers.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Linq;
+
+namespace Methods___Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+            Console.WriteLine(TheSmallestNum(num1, num2, num3));
+        }
+
+        static int TheSmallestNum(int num1, int num2, int num3)
+        {
+            int[] minValue = { num1, num2, num3 };
+            int min = minValue.Min();
+            return min;
+        }
+    }
+}
