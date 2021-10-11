d34060df66378696a000edaffbdba7a0c00495b4
Veseli Nedyalkov <91067756+VeselinNedyalkov@users.noreply.github.com>
2021-10-11 12:45:20 +0300

    Update 09. Palindrome Integers.cs

--- Fundamentals/Methods/09. Palindrome Integers.cs
+++ Fundamentals/Methods/09. Palindrome Integers.cs
@@ -1,28 +1,35 @@
-using System;
 
-namespace _08._Factorial_Division
+
+namespace _09._Palindrome_Integers
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int num1 = int.Parse(Console.ReadLine());
-            int num2 = int.Parse(Console.ReadLine());
-
-            double sum1 = Factorial(num1);
-            double sum2 = Factorial(num2);
-            double factorial = sum1 / sum2;
-            Console.WriteLine($"{factorial:f2}");
-        }
-
-        static double Factorial(int num)
-        {
-            double sum = 1;
-            for (int i = num; i >= 2; i--)
+            string inputNumber = Console.ReadLine();
+            
+            while (inputNumber != "END")
             {
-                sum *= i;
+                string reverceNum = string.Empty;
+                for (int i = inputNumber.Length - 1; i >= 0; i--)
+                {
+                    reverceNum += inputNumber[i];
+                }
+
+                bool answer = false;
+
+                if (inputNumber == reverceNum)
+                {
+                    answer = true;
+                }
+
+                Console.WriteLine(answer);
+                inputNumber = Console.ReadLine();
             }
-            return sum;
+            
+            
         }
+
+       
     }
 }
