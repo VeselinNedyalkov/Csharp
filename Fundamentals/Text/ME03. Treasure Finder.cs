2fb0dbb296bce74fb8d2dea364ed57c4a2c12436
Veseli Nedyalkov <91067756+VeselinNedyalkov@users.noreply.github.com>
2021-11-16 23:16:54 +0200

    Update ME03. Treasure Finder.cs

--- Fundamentals/Text/ME03. Treasure Finder.cs
+++ Fundamentals/Text/ME03. Treasure Finder.cs
@@ -9,48 +9,48 @@ namespace Treasure_Finder
     {
         static void Main(string[] args)
         {
-            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
-            List<string> codedWord = new List<string>(2);
-            string input = Console.ReadLine();
+            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();      //take the numbers for decoding
+            List<string> codedWord = new List<string>(2);                                   //create List to store the words
+            string input = Console.ReadLine();                                              //input the words
             
 
-            while (input != "find")
+            while (input != "find")                                                     //till find command come
             {               
-                codedWord.Add(input);
+                codedWord.Add(input);                                                       //add to the list
                 
                 input = Console.ReadLine();
             }
             
-            StringBuilder uncoding = new StringBuilder();
-            string[] lastWord = new string[4];
+            StringBuilder uncoding = new StringBuilder();                               //create SB
+            string[] lastWord = new string[4];                                          //create array for the last word
 
-            for (int i = 0; i < codedWord.Count; i++)
+            for (int i = 0; i < codedWord.Count; i++)                                   //for cicle for the LIST
             {
-                string workingWord = codedWord[i];
-                int k = 0;
-                int j = 0;
-                do
+                string workingWord = codedWord[i];                                      //take the word with index i
+                int k = 0;                                                              //counter for the numbers in numbers array
+                int j = 0;                                                              //counter for the word in workingWord
+                do                                                                      //do cicle
                 {
-                    int number = numbers[k];
-                    char temp = workingWord[j];
-                    char apeend = (char)(temp - number);
-                    uncoding.Append(apeend.ToString());
-                    j++;
-                    k++;
-                    if (k == numbers.Length)
+                    int number = numbers[k];                                            //number = the number with index k from number array
+                    char temp = workingWord[j];                                           //temp = char from the word with index j
+                    char apeend = (char)(temp - number);                               //remuve the number from the char to uncode the new char
+                    uncoding.Append(apeend.ToString());                                 //add the char to the SB
+                    j++;                                                    //increase j by one
+                    k++;                                                    // increase k by one
+                    if (k == numbers.Length)                                //if k is equal to the lenght of the array make it again 0
                     {
                         k = 0;
                     }
                 }
-                while (j != workingWord.Length);
+                while (j != workingWord.Length);                            //when j is equal to the lenght of the workingWord break;
 
-                uncoding.Replace('&', ' ').Replace('>',' ').Replace('<', ' ');
+                uncoding.Replace('&', ' ').Replace('>',' ').Replace('<', ' ');      //replace all simvols with space
 
-                string tempOne = uncoding.ToString().Trim();
-                lastWord = tempOne.Split(" ");
+                string tempOne = uncoding.ToString().Trim();                        //make the SB to string and trim it to remove space at the end
+                lastWord = tempOne.Split(" ");                                      //split it to take the resurse name and cordinates
 
-                Console.WriteLine($"Found {lastWord[1]} at {lastWord[lastWord.Length- 1]}");
-                uncoding.Clear();
+                Console.WriteLine($"Found {lastWord[1]} at {lastWord[lastWord.Length- 1]}");        //print
+                uncoding.Clear();                                                     //SB clear for the next input          
             }
            
         }
