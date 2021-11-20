1982b71159c3184721ca4b56d5c0b820bc5e1751
Veseli Nedyalkov <91067756+VeselinNedyalkov@users.noreply.github.com>
2021-11-20 09:22:33 +0200

    Update EX Race.cs

--- Fundamentals/Regular Expressions/EX Race.cs
+++ Fundamentals/Regular Expressions/EX Race.cs
@@ -9,28 +9,35 @@ namespace Race
     {
         static void Main(string[] args)
         {
+            //List that contains the names of the runers
             List<string> ImputNames = Console.ReadLine().Split(", ").ToList();
             string paternName = @"(?<name>[A-Za-z]+)";
             string paternNum = @"(?<num>[0-9]+)";
+            //dictionary to sort the winers
             Dictionary<string,int> runers = new Dictionary<string,int>();
 
             string input = Console.ReadLine();
 
             while (input != "end of race")
             {
+                //take coleection of letters from the input
                 MatchCollection matchName = Regex.Matches(input, paternName);
+                //take coleection of numbers from the input
                 MatchCollection matchNum = Regex.Matches(input,paternNum);
 
+                //join the collections
                 string name = string.Join("", matchName);
                 string totalRunKm = string.Join("", matchNum);
                 int sumKm = 0;
+                //sum all the km from the collection totalRunKm
                 for (int i = 0; i < totalRunKm.Length; i++)
                 {
                     sumKm += (int)char.GetNumericValue(totalRunKm[i]);
                 }
-
+                //check if the name exist in the 1st list with participant
                 if (ImputNames.Contains(name))
                 {
+                    //check if the name exist in the dictionary 
                     if (!runers.ContainsKey(name))
                     {
                         runers.Add(name, sumKm);
@@ -43,11 +50,11 @@ namespace Race
 
                 input = Console.ReadLine();
             }
-
-            runers = runers.OrderByDescending(x => x.Value).ToDictionary(x => x.Key,y => y.Value);
+            
+            //take the 1st 3 winers
             int count = 0;
             string[] winers = new string[3];
-            foreach (var runer in runers)
+            foreach (var runer in runers.OrderByDescending(x => x.Value))
             {
                 winers[count] = runer.Key;
                 count++;
