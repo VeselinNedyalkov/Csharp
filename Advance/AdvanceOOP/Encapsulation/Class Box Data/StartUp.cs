32b6e64c0bdae2e8e7d9255fc92470fab4e3709f
Veseli Nedyalkov <91067756+VeselinNedyalkov@users.noreply.github.com>
2022-01-18 02:43:19 +0200

    Create StartUp.cs

---
+++ Advance/AdvanceOOP/Encapsulation/Class Box Data/StartUp.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace Encapsulation
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            double lenght = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+
+            Box box = null;
+
+            try
+            {
+                box = new Box(lenght, width, height);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+
+            Console.WriteLine($"Surface Area - {box.SurfaceArea():f2}");
+            Console.WriteLine($"Lateral Surface Area - {box.LateralSurfaceArea():f2}");
+            Console.WriteLine($"Volume - {box.Volume():f2}");
+        }
+    }
+}
