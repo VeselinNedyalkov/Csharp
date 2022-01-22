0fc40c2bdb0440cef89830e7e1e41474e2c292db
Veseli Nedyalkov <91067756+VeselinNedyalkov@users.noreply.github.com>
2022-01-22 12:01:08 +0200

    Create Dough.cs

---
+++ Advance/AdvanceOOP/Encapsulation/Pizza Calories/Dough.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PizzaCalories
+{
+    public class Dough
+    {
+        private double flourType;
+        private double backingTechnique;
+        private double grams;
+        private double BaseCaloriesPerGram = 2;
+
+        public Dough(string flour, string backing,double grams)
+        {
+            Flour = flour;
+            Backing = backing;
+            Grams = grams;
+        }
+
+        public string Flour 
+        {
+            get => flourType.ToString();
+            set 
+            { 
+                if (value.ToLower() == "white")
+                {
+                    flourType = 1.5;
+                }
+                else if (value.ToLower() == "wholegrain")
+                {
+                    flourType = 1;
+                }
+                else
+                {
+                    throw new ArgumentException("Invalid type of dough.");
+                }
+            }
+                
+        }
+        public string Backing 
+        {
+            get => backingTechnique.ToString();
+            set
+            {
+                if (value.ToLower() == "crispy")
+                {
+                    backingTechnique = 0.9;
+                }
+                else if(value.ToLower() == "chewy")
+                {
+                    backingTechnique = 1.1;
+                }
+                else if (value.ToLower() == "homemade")
+                {
+                    backingTechnique = 1;
+                }
+                else
+                {
+                    throw new ArgumentException("Invalid type of dough.");
+                }
+            } 
+        }
+        public double Grams 
+        { 
+            get => grams;
+            set
+            {
+                if (value >= 1 && value <= 200)
+                {
+                    grams = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Dough weight should be in the range[1..200].");
+                }
+            }
+        }
+
+        public double CalculateCalories()
+        {
+            return (BaseCaloriesPerGram * grams) * backingTechnique * flourType;
+        }
+    }
+}
