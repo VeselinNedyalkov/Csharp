25f1aaece6047be8600118418fdb178938f3ec3c
Veseli Nedyalkov <91067756+VeselinNedyalkov@users.noreply.github.com>
2022-01-30 05:02:51 +0200

    Update StartUp.cs

--- Advance/AdvanceOOP/Polymorphism/Vehicles Extension/StartUp.cs
+++ Advance/AdvanceOOP/Polymorphism/Vehicles Extension/StartUp.cs
@@ -1,120 +1,67 @@
 using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace Vehicles
 {
-    public class Program
+    public abstract class Vehical : IVehical
     {
-        static void Main(string[] args)
+        private double fuelQuntity;
+
+        protected Vehical(double fuelQuntity, double fuelConsuption, double tankCapacity)
         {
-            string[] carInfo = Console.ReadLine().Split();
-            string[] truckInfo = Console.ReadLine().Split();
-            string[] buskInfo = Console.ReadLine().Split();
+            TankCapacity = tankCapacity;
+            FuelQuntity = fuelQuntity;
+            FuelConsuption = fuelConsuption;
+            IsEmpty = true;
+        }
 
-            double carFuel = double.Parse(carInfo[1]);
-            double carConsumptio = double.Parse(carInfo[2]);
-            double carTankCap = double.Parse(carInfo[3]);
-            
-            double truckFuel = double.Parse(truckInfo[1]);
-            double truckConsumptio = double.Parse(truckInfo[2]);
-            double truckTankCap = double.Parse(truckInfo[3]);
-            
-            double busFuel = double.Parse(buskInfo[1]);
-            double busConsumptio = double.Parse(buskInfo[2]);
-            double busTankCap = double.Parse(buskInfo[3]);
-
-
-
-            IVehical car = new Car(carFuel, carConsumptio, carTankCap);
-            IVehical truck = new Truck(truckFuel, truckConsumptio, truckTankCap);
-            IVehical bus = new Bus(busFuel, busConsumptio, busTankCap);
-
-            int num = int.Parse(Console.ReadLine());
-
-            for (int i = 0; i < num; i++)
+        public double FuelQuntity 
+        { 
+            get => fuelQuntity; 
+            set
             {
-                string[] comands = Console.ReadLine().Split();
-                string cmd = comands[0];
-                string vehical = comands[1];
-                double value = double.Parse(comands[2]);
-               
-                switch (cmd)
+                if (value > TankCapacity)
                 {
-                    case "Drive":
-                        if (vehical == "Car")
-                        {
-                            if (car.EnoughtFuel(value))
-                            {
-                                car.Drive(value);
-                                Console.WriteLine($"Car travelled {value} km");
-                            }
-                            else
-                                Console.WriteLine("Car needs refueling");
-                        }
-                        else if(vehical == "Truck")
-                        {
-                            if (truck.EnoughtFuel(value))
-                            {
-                                truck.Drive(value);
-                                Console.WriteLine($"Truck travelled {value} km");
-                            }
-                            else
-                                Console.WriteLine("Truck needs refueling");
-                        }
-                        else if (vehical == "Bus")
-                        {
-                            if (bus.EnoughtFuel(value))
-                            {
-                                bus.IsEmpty = false;
-                                bus.Drive(value);
-                                Console.WriteLine($"Bus travelled {value} km");
-                            }
-                            else
-                                Console.WriteLine("Bus needs refueling");
-                        }
-                        break;
-
-                    case "Refuel":
-                        try
-                        {
-                            if (vehical == "Car")
-                            {
-                                car.Refiling(value);
-                            }
-                            else if (vehical == "Truck")
-                            {
-                                truck.Refiling(value);
-                            }
-                            else if (vehical == "Bus")
-                            {
-                                bus.Refiling(value);
-                            }
-                        }
-                        catch (Exception ex)
-                        {
-                            Console.WriteLine(ex.Message);
-                        }
-                        break;
-
-                    case "DriveEmpty":
-                        if (bus.EnoughtFuel(value))
-                        {
-                            bus.IsEmpty = true;
-                            bus.Drive(value);
-                            Console.WriteLine($"Bus travelled {value} km");
-                        }
-                        else
-                            Console.WriteLine("Bus needs refueling");
-                        break;
-
-                    default:
-                        break;
+                    fuelQuntity = 0;
+                  
                 }
+                else
+                    fuelQuntity = value;
 
-            }//for
+            } 
+        }
 
-            Console.WriteLine($"Car: {car.FuelQuntity:f2}");
-            Console.WriteLine($"Truck: {truck.FuelQuntity:f2}");
-            Console.WriteLine($"Bus: {bus.FuelQuntity:f2}");
+        public virtual double FuelConsuption { get; set; }
+        public double TankCapacity { get; set; }
+        public bool IsEmpty { get; set; }
+
+        public bool EnoughtFuel(double distance)
+            => distance * FuelConsuption < FuelQuntity;
+
+
+        public void Drive(double distance)
+        {
+            FuelQuntity -= distance * FuelConsuption;
+        }
+
+        public virtual void Refiling(double quantity)
+        {
+            if (quantity <= 0)
+            {
+                throw new ArgumentException("Fuel must be a positive number");
+            }
+
+            ValidateQuantity(quantity);
+            FuelQuntity += quantity;
+        }
+
+        protected void ValidateQuantity(double quantity)
+        {
+            if (quantity + FuelQuntity > TankCapacity)
+            {
+                throw new ArgumentException($"Cannot fit {quantity} fuel in the tank");
+            }
         }
     }
 }
