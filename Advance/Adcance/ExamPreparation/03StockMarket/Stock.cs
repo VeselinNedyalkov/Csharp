ffaf5e907b55f3cac367840032c335f668ee1eef
Veseli Nedyalkov <91067756+VeselinNedyalkov@users.noreply.github.com>
2022-02-06 06:01:06 +0200

    Create Stock.cs

---
+++ Advance/Adcance/ExamPreparation/03StockMarket/Stock.cs
@@ -0,0 +1,41 @@
+using System.Text;
+
+namespace StockMarket
+{
+    public class Stock
+    {
+        private string companyName;
+        private string director;
+        private decimal pricePerShare;
+        private int totalNumberOfShares;
+        private decimal marketCapitalization;
+
+        public Stock(string companyName, string director, decimal pricePerShare, int totalNumberOfShares)
+        {
+            CompanyName = companyName;
+            Director = director;
+            PricePerShare = pricePerShare;
+            TotalNumberOfShares = totalNumberOfShares;
+            MarketCapitalization = pricePerShare * totalNumberOfShares;
+        }
+
+        public string CompanyName { get => companyName; set => companyName = value; }
+        public string Director { get => director; set => director = value; }
+        public decimal PricePerShare { get => pricePerShare; set => pricePerShare = value; }
+        public int TotalNumberOfShares { get => totalNumberOfShares; set => totalNumberOfShares = value; }
+        public decimal MarketCapitalization { get => marketCapitalization; set => marketCapitalization = value; }
+
+        public override string ToString()
+        {
+
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Company: {CompanyName}");
+            sb.AppendLine($"Director: {Director}");
+            sb.AppendLine($"Price per share: ${PricePerShare}");
+            sb.AppendLine($"Market capitalization: ${MarketCapitalization}");
+
+            return sb.ToString().Trim();
+        }
+
+    }
+}
