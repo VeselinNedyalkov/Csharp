6cbeb0b59ebffbb5ae71a542151d515bcab5a55b
Veseli Nedyalkov <91067756+VeselinNedyalkov@users.noreply.github.com>
2022-07-02 15:31:16 +0300

    Create 5.	Employees from Research and Development.cs

---
+++ DataBase/EntityFrameworkCore/5.	Employees from Research and Development.cs
@@ -0,0 +1,45 @@
+using SoftUni.Data;
+using SoftUni.Models;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            using SoftUniContext dbContext = new SoftUniContext();
+
+            string answer = GetEmployeesFromResearchAndDevelopment(dbContext);
+
+            Console.WriteLine(answer);
+        }
+
+        public static string GetEmployeesFromResearchAndDevelopment(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var answer = context.Employees
+                 .Where(e => e.Department.Name == "Research and Development ")
+                 .Select(e => new
+                 {
+                     e.FirstName,
+                     e.LastName,
+                     DepartmentName = e.Department.Name,
+                     e.Salary
+                 })
+                 .OrderBy(e => e.Salary)
+                 .ThenByDescending(e => e.FirstName)
+                 .ToArray();
+
+            foreach (var e in answer)
+            {
+                sb.AppendLine($"{e.FirstName} {e.LastName} from {e.DepartmentName} - {e.Salary:f2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
