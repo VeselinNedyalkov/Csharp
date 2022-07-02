b21d7eb782bbbd32f75009b9d6f34531c13fc21a
Veseli Nedyalkov <91067756+VeselinNedyalkov@users.noreply.github.com>
2022-07-02 15:02:49 +0300

    Create 4.	Employees with Salary Over 50 000.cs
    Use the same Model and Data like  3.	Employees Full Information

---
+++ DataBase/EntityFrameworkCore/4.	Employees with Salary Over 50 000.cs
@@ -0,0 +1,43 @@
+using SoftUni.Data;
+using SoftUni.Models;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            SoftUniContext dbContext = new SoftUniContext();
+
+            string answer = GetEmployeesFullInformation(dbContext);
+
+            Console.WriteLine(answer);
+        }
+
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+           var allEmployees = context.Employees
+                .OrderBy(e => e.EmployeeId)
+                .Select(e => new
+                { 
+                    e.FirstName,
+                    e.Salary
+                })
+                .Where(e => e.Salary > 50000)
+                .ToArray()
+                .OrderBy(e => e.FirstName);
+
+            foreach (var e in allEmployees)
+            {
+                sb.AppendLine($"{e.FirstName} - {e.Salary:f2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
