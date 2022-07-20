dd9f7a1ba24107326419545a290226d7e7f5b54d
Veseli Nedyalkov <91067756+VeselinNedyalkov@users.noreply.github.com>
2022-07-20 20:45:57 +0300

    Add files via upload

---
+++ DataBase/EntityFrameworkCore/JSON/ProductsShop/Datasets/DataTransfer/CategoriesInputModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductShop.Datasets.DataTransfer
+{
+    public class CategoriesInputModel
+    {
+        public string Name { get; set; }
+    }
+}
