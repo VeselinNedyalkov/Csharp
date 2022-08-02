73d40989539514d671e9856bf08e70a3fef3e8b4
Veseli Nedyalkov <91067756+VeselinNedyalkov@users.noreply.github.com>
2022-08-02 08:14:01 +0300

    Add files via upload

---
+++ DataBase/EntityFrameworkCore/ExamPreparation/SoftJail/DataProcessor/Deserializer.cs
@@ -0,0 +1,75 @@
+﻿namespace SoftJail.DataProcessor
+{
+
+    using Data;
+    using Newtonsoft.Json;
+    using SoftJail.DataProcessor.ImportDto;
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Text;
+    using SoftJail.Data.Models;
+    using System.Xml.Linq;
+
+    public class Deserializer
+    {
+        public static string ImportDepartmentsCells(SoftJailDbContext context, string jsonString)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            IEnumerable<DepartmentsCellsImportModel> departmentsCells =
+                JsonConvert.DeserializeObject<IEnumerable<DepartmentsCellsImportModel>>
+                 (jsonString);
+
+            List<Department> departments = new List<Department>();
+
+            foreach (var dep in departmentsCells)
+            {
+                if (!IsValid(dep) || !dep.Cells.Any() || !dep.Cells.All(IsValid))
+                {
+                    sb.AppendLine("Invalid Data");
+                    continue;
+                }
+
+                Department department = new Department
+                {
+                    Name = dep.Name,
+                    Cells = dep.Cells.Select(x => new Cell
+                    {
+                        CellNumber = x.CellNumber,
+                        HasWindow = x.HasWindow
+                    })
+                    .ToList()
+                };
+
+                sb.AppendLine($"Imported {department.Name} with {department.Cells.Count} cells");
+                departments.Add(department);
+            }
+
+            context.Departments.AddRange(departments);
+            context.SaveChanges();
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public static string ImportPrisonersMails(SoftJailDbContext context, string jsonString)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string ImportOfficersPrisoners(SoftJailDbContext context, string xmlString)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static bool IsValid(object obj)
+        {
+            var validationContext = new System.ComponentModel.DataAnnotations.ValidationContext(obj);
+            var validationResult = new List<ValidationResult>();
+
+            bool isValid = Validator.TryValidateObject(obj, validationContext, validationResult, true);
+            return isValid;
+        }
+    }
+}
---
+++ DataBase/EntityFrameworkCore/ExamPreparation/SoftJail/DataProcessor/ImportDto/PrisonersMailsImportModel.cs
@@ -0,0 +1,54 @@
+﻿using SoftJail.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftJail.DataProcessor.ImportDto
+{
+    public class PrisonersMailsImportModel
+    {
+        public PrisonersMailsImportModel()
+        {
+            Mails = new HashSet<MailImportModel>();
+        }
+        public string Name { get; set; }
+        public string Nickname { get; set; }
+        public int age { get; set; }
+        public string IncarcerationDate { get; set; }
+        public string ReleaseDate { get; set; }
+        public decimal Bail { get; set; }
+        public int CellId { get; set; }
+
+        public ICollection<MailImportModel> Mails { get; set; }
+
+    }
+
+    public class MailImportModel
+    {
+
+    }
+}
+
+
+
+//{
+//    "FullName": "",
+//    "Nickname": "The Wallaby",
+//    "Age": 32,
+//    "IncarcerationDate": "29/03/1957",
+//    "ReleaseDate": "27/03/2006",
+//    "Bail": null,
+//    "CellId": 5,
+//    "Mails": [
+//      {
+//        "Description": "Invalid FullName",
+//        "Sender": "Invalid Sender",
+//        "Address": "No Address"
+//      },
+//      {
+//        "Description": "Do not put this in your code",
+//        "Sender": "My Ansell",
+//        "Address": "ha-ha-ha"
+//      }
+//    ]
+//  },
