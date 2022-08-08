93639848ca2a3ee24ba802e09dd8f10a910d92c3
Veseli Nedyalkov <91067756+VeselinNedyalkov@users.noreply.github.com>
2022-08-08 15:01:30 +0300

    Update Tasks.sql

--- DataBase/T-SQL/ExamPreparation/WMS/Tasks.sql
+++ DataBase/T-SQL/ExamPreparation/WMS/Tasks.sql
@@ -29,4 +29,36 @@ SELECT c.FirstName + ' ' + c.LastName,
 ORDER BY d DESC , c.ClientId
 
 --07. Mechanic Performance
+SELECT m.FirstName + ' ' + m.LastName,
+	AVG(DATEDIFF(DAY, [IssueDate] , [FinishDate] ))
+	FROM [dbo].[Mechanics] AS m
+	JOIN Jobs AS j ON j.MechanicId = m.MechanicId
+	GROUP BY j.MechanicId , m.FirstName + ' ' + m.LastName
+	ORDER BY j.MechanicId
+	
+--08. Available Mechanics
+--SELECT  m.FirstName + ' ' + m.LastName
+-- FROM Mechanics AS m
+-- LEFT JOIN Jobs AS j ON j.MechanicId = m.MechanicId
+-- WHERE j.JobId IS NULL OR (SELECT COUNT(JobId)
+--						FROM Jobs
+--						Where [Status]  != 'Finished' AND MechanicId = m.MechanicId
+--						GROUP BY MechanicId, [Status]) IS NULL
+--GROUP BY m.MechanicId, m.FirstName + ' ' + m.LastName
 
+   SELECT m.FirstName + ' ' + m.LastName AS fullName
+     FROM [Mechanics] AS m
+LEFT JOIN Jobs AS j ON j.MechanicId = m.MechanicId
+    WHERE j.Status = 'Finished' AND j.Status IS NULL
+	GROUP BY m.FirstName + ' ' + m.LastName , m.MechanicId
+	ORDER BY m.MechanicId
+	
+--09. Past Expenses
+SELECT j.JobId, ISNULL(SUM(p.Price * op.Quantity),0) AS TotalOrder
+	FROM Jobs as j
+	LEFT JOIN Orders AS o ON o.JobId = j.JobId
+	LEFT JOIN OrderParts AS op ON op.OrderId = o.OrderId
+	LEFT JOIN Parts AS p ON p.PartId = op.PartId
+	WHERE Status = 'Finished'
+	GROUP BY j.JobId
+	ORDER BY TotalOrder DESC , j.JobId
